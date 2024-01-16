2dae6841e38c537c4b9fe3e9cd5f3013824c54ee
Marc Paterno <paterno@fnal.gov>
2024-01-16 10:28:42 -0600

    First working atan2_1

--- GNUmakefile
+++ GNUmakefile
@@ -1,8 +1,12 @@
-all: january-2023.pdf
+all: january-2023.pdf atan2.pdf
 
 january-2023.pdf: january-2023.qmd vtune_data.csv
+	quarto render $< --to beamer
+atan2.pdf: atan2.qmd atan2.tsv
 	quarto render $< --to beamer
 
 clean:
 	rm -f january-2023.pdf january-2023.tex
 	rm -rf january-2023_cache/* january-2023_files/*
+	rm -f atan2.pdf atan2.tex
+	rm -rf atan_cache/* atan2_files/*
--- fast_atan_t.cc
+++ fast_atan_t.cc
@@ -5,79 +5,73 @@
 
 #include "nanobench.h"
 
-double atan2d(double y, double x)
-{
-  return std::atan2(y, x);
-}
+double atan2d(double y, double x) { return std::atan2(y, x); }
 
-inline double atan_aux2(double z)
-{
+inline double atan_aux2(double z) {
   // Note: 0 < z <= 1.
-  double const a0 = 0.78539816;
-  double const a1 = 0.24434610;
-  double const a2 = 0.06684611;
-  return z * (a0 - (z - 1)*(a1 + a2*z));
+  double const a0 = 7.84086493111993965e-01;
+  double const a1 = 2.43049810801771404e-01;
+  double const a2 = 7.67849627218896019e-02;
+  return z * (a0 - (z - 1) * (a1 + a2 * z));
 }
 
-inline double atan2_aux(double y, double x)
-{
-  // Note: x > 0 and y > 0.
-  double const z = y/x;
-  if ( z <= 1.) return atan_aux2(z);
-  return M_PI_2 - atan_aux2(1/z);
+inline double atan2_aux(double z) {
+  bool const negative = (z < 0.);
+  double abz = std::fabs(z);
+  double tmp = (abz <= 1.) ? atan_aux2(abz) : M_PI_2 - atan_aux2(1. / abz);
+  if (z >= 0.)
+    return tmp;
+  return -tmp;
 }
 
-double atan2_1(double y, double x)
-{
-  if (x > 0) return atan2_aux(y, x);
-  if (x < 0)
-  {
-    if (y >= 0) return atan2_aux(y, x) + M_PI;
-    return atan2_aux(y, x) - M_PI;
+double atan2_1(double y, double x) {
+  if (x > 0)
+    return atan2_aux(y / x);
+  if (x < 0) {
+    if (y >= 0)
+      return M_PI + atan2_aux(y / x);
+    return -M_PI + atan2_aux(y / x);
   }
   // We now know x == 0.0
-  if (y > 0) return M_PI_2;
-  if (y < 0) return -M_PI_2;
+  if (y > 0)
+    return M_PI_2;
+  if (y < 0)
+    return -M_PI_2;
   // We now know y == 0
   return std::numeric_limits<double>::quiet_NaN();
 }
 
 template <typename F>
-void
-run_bench(F func, ankerl::nanobench::Bench* bench, char const* name)
-{
+void run_bench(F func, ankerl::nanobench::Bench *bench, char const *name) {
   volatile double x = 0.457;
   volatile double y = -0.57;
   bench->run(name, [&]() {
-      double z = func(y, x);
-      ankerl::nanobench::doNotOptimizeAway(z);
-      });
+    double z = func(y, x);
+    ankerl::nanobench::doNotOptimizeAway(z);
+  });
 }
 
 void bmark() {
   ankerl::nanobench::Bench b;
   b.title("atan tests")
-    .performanceCounters(true)
-    .minEpochIterations(1 * 1000 * 1000);
+      .performanceCounters(true)
+      .minEpochIterations(1 * 1000 * 1000);
   run_bench(&atan2d, &b, "atan2d");
   run_bench(&atan2_1, &b, "atan2_1");
 }
 
 int main() {
-  int const npoints = 10 * 1000;
-  double const ymin = 0.0;
-  double const ymax = 1.0;
+  int const npoints = 40 * 1000;
+  double const ymin = -4.0;
+  double const ymax = 4.0;
   double dy = (ymax - ymin) / npoints;
-  std::cerr << "x\tatan2d\n" << std::setprecision(17);
+  std::cerr << "x\tatan2d\tatan2_1\n" << std::setprecision(17);
 
-  for (int i = 0; i != npoints+1; ++i) {
-    // double const y = ymin + i * dy;
-    double const frac = static_cast<double>(i)/npoints;
-    double const y = ymax * frac; // we know that ymin is zero
-    std::cerr << y 
-      << '\t' << atan2d(y, 1.0)
-      << '\t' << atan2_1(y, 1.0)
-      << '\n';
+  for (int i = 0; i != npoints + 1; ++i) {
+    double const y = ymin + i * dy;
+    // double const frac = static_cast<double>(i)/npoints;
+    // double const y = ymax * frac; // we know that ymin is zero
+    std::cerr << y << '\t' << atan2d(y, 1.0) << '\t' << atan2_1(y, 1.0) << '\n';
   }
 
   bmark();
