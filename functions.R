6e09eb6f823d3bde0513785c1fef1f3b29b2eb03
Marc Paterno <paterno@fnal.gov>
2024-02-19 13:25:40 -0600

    Augment initial version of physics validation #2

--- functions.R
+++ functions.R
@@ -116,4 +116,33 @@ make_analysis_dataframes <- function(orig_channels, orig_meas)
 
   channels <- inner_join(channels, tmp)
   list(channels = channels, meas = meas)
+
+#' Augment a dataframe with columns to allow plotting of conditioned box plots.
+#'
+#' @param df a dataframe
+#' @param var a tidy-select column specification
+#' @param nbins the number of bins to generate
+#'
+#' @return a tibble with two new columns, for the var's bin number and the var's
+#'          median
+#' @export
+#'
+augment_bins <- function(df, var, nbins=25)
+{
+  # Capture the argument as a quosure
+  v <- enquo(var)
+  # Get the string representation of the variable
+  vname <- rlang::as_string(rlang::quo_get_expr(v))
+  # add a new column named bin_column_name carrying the bin number for each row.
+  tmp <- mutate(df, ibin = cut_number(!!v, nbins, labels=FALSE))
+  # Determine the median of the variable var in each bin.
+  tmp2 <- group_by(tmp, ibin) |>
+    summarize(xxx = median(!!v), .groups="drop")
+  # Give the new column a more appropriate name.
+  names(tmp2)[length(tmp2)] <- paste0(vname, "_median")
+  # Now augment the original dataframe with the new column
+  tmp <- left_join(tmp, tmp2, by="ibin")
+  names(tmp)[length(tmp)-1] <- paste0(vname, "_bin")
+  tmp
+}
 }
